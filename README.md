8bec4cf3d7f119ae6b4390073983a1c404f94f58
EugeneStephen <devmiguel08@gmail.com>
2022-07-14 17:17:57 +0300

    another one

---
+++ README.md
@@ -0,0 +1,4 @@
+# Latest Repo
+im glad to present yet another product in the PassionFruit line up,
+the Passion pro plus max ultra  5g
+find link if the description below
---
+++ TheJS.js
@@ -0,0 +1,8 @@
+<script>
+    var name="james_Stewart";
+    var Age=19;
+    Var grade="A"
+    console.log(name);
+    console.log(Age);
+    console.log(grade)
+    </script>
